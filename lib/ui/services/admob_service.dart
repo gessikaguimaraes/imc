efc0e2d066c4962c9147641c5f5e9fd26c9d3f27
Géssika <gessika.156@gmail.com>
2020-08-01 14:54:13 -0300

    Ajustes

---
+++ lib/ui/services/admob_service.dart
@@ -0,0 +1,57 @@
+import 'package:firebase_admob/firebase_admob.dart';
+import 'package:flutter/material.dart';
+
+class AdMobService {
+  MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+    keywords: <String>[
+      "Games",
+      "beautiful apps",
+      "imc",
+      "IMC",
+    ],
+    // contentUrl: 'https://flutter.io',
+    childDirected: false,
+    testDevices: <String>[],
+  );
+  BannerAd bannerAd;
+  InterstitialAd interstitialAd;
+
+  BannerAd createBannerAd() {
+    return BannerAd(
+      adUnitId: BannerAd.testAdUnitId,
+      size: AdSize.smartBanner,
+      targetingInfo: targetingInfo,
+      listener: (MobileAdEvent event) {
+        //print("BannerAd event is $event");
+      },
+    );
+  }
+
+  InterstitialAd createInterstitialAd(BuildContext context) {
+    return InterstitialAd(
+      adUnitId: InterstitialAd.testAdUnitId,
+      targetingInfo: targetingInfo,
+      listener: (MobileAdEvent event) {
+        /*  if (event == MobileAdEvent.opened ||
+              event == MobileAdEvent.failedToLoad)
+            Navigator.pushNamed(context, "/opcoes"); */
+      },
+    );
+  }
+
+  void mostrarBanner() {
+    bannerAd = createBannerAd()
+      ..load()
+      ..show();
+  }
+
+  void mostrarInterstitial(BuildContext context) {
+    interstitialAd = createInterstitialAd(context)
+      ..load()
+      ..show(
+        anchorType: AnchorType.bottom,
+        anchorOffset: 0.0,
+        horizontalCenterOffset: 0.0,
+      );
+  }
+}
