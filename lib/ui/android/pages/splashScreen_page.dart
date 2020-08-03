743d6ab7d64e8d550a36434307e4423b2c393e8e
Géssika <gessika.156@gmail.com>
2020-08-03 13:46:05 -0300

    Ajuste Splash Screen

--- lib/ui/android/pages/splashScreen_page.dart
+++ lib/ui/android/pages/splashScreen_page.dart
@@ -9,30 +9,22 @@ class SplashScreenPage extends StatefulWidget {
   _SplashScreenPageState createState() => _SplashScreenPageState();
 }
 
-class _SplashScreenPageState extends State<SplashScreenPage>
-    with TickerProviderStateMixin {
-  AnimationController _controller;
-
+class _SplashScreenPageState extends State<SplashScreenPage> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 5)).then((value) {
+    /*  Future.delayed(Duration(seconds: 5)).then((value) {
       Navigator.push(
         context,
         MaterialPageRoute(
           builder: (context) => SexoPage(),
         ),
       );
-    });
-    _controller = AnimationController(
-      duration: const Duration(seconds: 10),
-      vsync: this,
-    )..repeat();
+    }); */
   }
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
   }
 
@@ -40,38 +32,22 @@ class _SplashScreenPageState extends State<SplashScreenPage>
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      //backgroundColor: Color(0xff93291E),
-      body: Stack(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [Color(0xffED213A), Color(0xff93291E)],
-              ),
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Color(0xffED213A), Color(0xff93291E)],
           ),
-          SpinningContainer(controller: _controller),
-        ],
-      ),
-    );
-  }
-}
-
-class SpinningContainer extends AnimatedWidget {
-  const SpinningContainer({Key key, AnimationController controller})
-      : super(key: key, listenable: controller);
-
-  Animation<double> get _progress => listenable;
-
-  @override
-  Widget build(BuildContext context) {
-    return Transform.rotate(
-      angle: _progress.value * 2.0 * math.pi,
-      child: Container(
-        alignment: Alignment.center,
-        child: Image.asset("images/logo.png"),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("images/logo.png"),
+            Image.asset("images/logo.png")
+          ],
+        ),
       ),
     );
   }
